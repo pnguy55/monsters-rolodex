bd027798bd48cc46a222b934a31152c546be76cd
Phi Nguyen <pnguy55@gmail.com>
2020-06-11 18:20:32 -0500

    add users

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edits <code>src/App.js</code> and saves to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      monsters: []
+    };
+  }
+
+  componentDidMount(){
+    fetch('http://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map(monster => (
+          <h1 key = {monster.id}> {monster.name} </h1>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
